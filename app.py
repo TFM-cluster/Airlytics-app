aa8d132a3ebe982fff2fb3ae8c22ffb69609b54e
Airlytics <iwaix@tfm.co.jp>
2025-07-31 12:32:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.image(tfm_logo, width=100)
 st.markdown(
     """
     <div style='text-align: center; color: #3399cc; font-family: Meiryo, sans-serif;
-                font-size: 26pt; font-weight: bold; margin-top: 10px; margin-bottom: -5px;'>
+                font-size: 24pt; font-weight: bold; margin-top: 10px; margin-bottom: -5px;'>
         ラジオの空気を可視化する、エアリティクス
     </div>
     """,
