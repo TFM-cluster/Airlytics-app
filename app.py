b7e0e6ccf565a51187a662a964af5ac14e28196f
Airlytics <iwaix@tfm.co.jp>
2025-08-29 18:43:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit.components.v1 as components
 # ✅ ページ設定
 st.set_page_config(
     page_title="AIrlytics",
-    page_icon="📻",
+    page_icon=Image.open("AIrlytics.png")
     layout="centered",
     initial_sidebar_state="auto"
 )
@@ -146,5 +146,6 @@ for cid in sorted(cluster_info.keys()):
             st.image(Image.open(info["img"]), caption=f"クラスター{cid}のイメージ", width=300)
         except FileNotFoundError:
             st.warning(f"⚠️ 画像ファイル『{info['img']}』が見つかりません。")
+
 
 
